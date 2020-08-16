32abfb75121ca7146608bf85da7fd74981307e20
kawsar007 <imkawsar007@gmail.com>
2020-08-16 04:28:49 -0700

    netflix-clone-app

---
+++ src/project-info.js
@@ -0,0 +1,9 @@
+// api-key: 52808459063de060da1cfca53106356f
+
+// URL: https://api.themoviedb.org/3/movie/550?api_key=52808459063de060da1cfca53106356f
+
+/* Token: eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MjgwODQ1OTA2M2RlMDYwZGExY2ZjYTUzMTA2MzU2ZiIsInN1YiI6IjVmMzU1OTIxMTExZGExMDAzNT
+           Y1MzFjYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5RDlE719ZUYFVcQN4sedRVZNWCG6QVEh2TwDiEt7zm0 */
+
+
+// App URL: netflix-clone-cfa62.web.app
